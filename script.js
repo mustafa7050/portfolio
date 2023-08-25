8244eb7bda819a3e47a99e94d422421c1c348157
mustafa7050 <106078847+mustafa7050@users.noreply.github.com>
2023-08-26 02:18:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var typed = new Typed('#element', {
-    strings: ['Cyber Security Expert', 'Networking Engineer', 'IT Professional'],
-    typeSpeed: 50,
-  });
+document.querySelector(".view-resume-btn").addEventListener("click", function () {
+    window.open("resume/JavedIqbalResume.pdf", "_blank");
+});
 
-document.querySelector(".certification").addEventListener("click", function() {
+document.querySelector(".certification").addEventListener("click", function () {
     window.open("https://learn.microsoft.com/en-us/users/javediqbal-5910/transcript/dr995sz5o4nno0g?source=docs", "_blank");
 });
 
-document.querySelector(".view-resume-btn").addEventListener("click", function() {
-    window.open("resume/JavedIqbalResume.pdf", "_blank");
+var typed = new Typed('#element', {
+    strings: ['Cyber Security Expert', 'Networking Engineer', 'IT Professional'],
+    typeSpeed: 50,
 });
